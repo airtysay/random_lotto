7c8ef991a3ba77ac9a3d9c5b4b0758ac9b6d6808
airtysay <105341001+airtysay@users.noreply.github.com>
2022-09-16 13:33:28 +0900

    v1.0.1 Lotto Ui 생성
    기본 UI 생성 타이틀,텍스트 박스, 클릭 버튼

--- RandomLotto_WIn10/RandomLotto_WIn10/Form1.cs
+++ RandomLotto_WIn10/RandomLotto_WIn10/Form1.cs
@@ -6,5 +6,15 @@ namespace RandomLotto_WIn10
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
